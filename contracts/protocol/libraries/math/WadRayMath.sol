003e838fe27fe9a780cd098a82aa1578fdbb35db
TheMj0ln1r <charan.nomula.cn@gmail.com>
2026-04-27 22:01:39 +0530

    fix: update natspec

--- contracts/protocol/libraries/math/WadRayMath.sol
+++ contracts/protocol/libraries/math/WadRayMath.sol
@@ -94,17 +94,17 @@ library WadRayMath {
   /**
    * @notice Divides two ray, rounding up to the nearest ray
    * @dev assembly optimized for improved gas savings
+   * @dev copied from aave-v3-origin: https://github.com/aave-dao/aave-v3-origin/blob/1e3d70c4151a94166ebc59e2eaa4aff6e6ba6978/src/contracts/protocol/libraries/math/WadRayMath.sol#L114-L123
    * @param a Ray
    * @param b Ray
    * @return c = a raydiv b, rounded up
    */
   function rayDivCeil(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    // to avoid overflow, a <= type(uint256).max / RAY
     assembly {
+      // Overflow check: Ensure a * RAY does not exceed uint256 max
       if or(iszero(b), iszero(iszero(gt(a, div(not(0), RAY))))) {
         revert(0, 0)
       }
-
       let scaled := mul(a, RAY)
       c := add(div(scaled, b), iszero(iszero(mod(scaled, b))))
     }
@@ -113,16 +113,17 @@ library WadRayMath {
   /**
    * @notice Divides two ray, rounding down (truncating) to the nearest ray
    * @dev assembly optimized for improved gas savings
+   * @dev copied from aave-v3-origin: https://github.com/aave-dao/aave-v3-origin/blob/1e3d70c4151a94166ebc59e2eaa4aff6e6ba6978/src/contracts/protocol/libraries/math/WadRayMath.sol#L125-L133
    * @param a Ray
    * @param b Ray
    * @return c = a raydiv b, rounded down
    */
   function rayDivFloor(uint256 a, uint256 b) internal pure returns (uint256 c) {
     assembly {
+      // Overflow check: Ensure a * RAY does not exceed uint256 max
       if or(iszero(b), iszero(iszero(gt(a, div(not(0), RAY))))) {
         revert(0, 0)
       }
-
       c := div(mul(a, RAY), b)
     }
   }
