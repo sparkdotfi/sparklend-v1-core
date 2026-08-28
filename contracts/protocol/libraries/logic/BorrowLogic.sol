5d8dd91bcdcdc0d8600d2fa15bbceec864757a74
Michael De Luca <michael.deluca@circle-free.com>
2026-08-28 11:52:43 -0400

    fix: UseAsCollateral on repayment with ATokens (#27)

--- contracts/protocol/libraries/logic/BorrowLogic.sol
+++ contracts/protocol/libraries/logic/BorrowLogic.sol
@@ -263,6 +263,12 @@ library BorrowLogic {
         reserveCache.nextLiquidityIndex
       );
 
+      bool isCollateral = userConfig.isUsingAsCollateral(reserve.id);
+
+      if (isCollateral && IAToken(reserveCache.aTokenAddress).scaledBalanceOf(msg.sender) == 0) {
+        userConfig.setUsingAsCollateral(reserve.id, false);
+      }
+
       ValidationLogic.validateHealthFactor(
         reservesData,
         reservesList,
